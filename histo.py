6fc527d57d530e3587e6eb580af732fafc27bcff
GDamen <gabriele.damen@cern.ch>
2023-10-16 11:46:43 -0400

    Update histo.py

--- histo.py
+++ histo.py
@@ -4,21 +4,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #df_results = pd.DataFrame(columns = columnnames, dtype='float64')
-general_path = '/home/gdamen/projects/EDIT2023/'
+general_path = '.'
 
-output_path = general_path + 'pandas_df/'  # Output path for dataframe
+output_path = general_path  # Output path for dataframe
 df_results = pd.read_csv(os.path.join(output_path, 'results.csv'))
 
 binning = 50
 fig, axs = plt.subplots(2, 4)
-axs[0, 0].hist(df_results['Amp1 [mV]'].dropna(), bins=binning, range = [0, 150], histtype='step', density=True)
-axs[0, 1].hist(df_results['Amp2 [mV]'].dropna(), bins=binning, range = [0, 150], histtype='step', density=True)
-axs[0, 2].hist(df_results['Amp3 [mV]'].dropna(), bins=binning, range = [0, 150], histtype='step', density=True)
-axs[0, 3].hist(df_results['Amp4 [mV]'].dropna(), bins=binning, range = [0, 150], histtype='step', density=True)
-axs[1, 0].hist(df_results['Amp5 [mV]'].dropna(), bins=binning, range = [0, 150], histtype='step', density=True)
-axs[1, 1].hist(df_results['Amp6 [mV]'].dropna(), bins=binning, range = [0, 150], histtype='step', density=True)
-axs[1, 2].hist(df_results['Amp7 [mV]'].dropna(), bins=binning, range = [0, 150], histtype='step', density=True)
-axs[1, 3].hist(df_results['Amp8 [mV]'].dropna(), bins=binning, range = [0, 150], histtype='step', density=True)
+axs[0, 0].hist(df_results['Amp1[mV]'].dropna(), bins=binning, range = [0, 150], histtype='step', density=True)
+axs[0, 1].hist(df_results['Amp2[mV]'].dropna(), bins=binning, range = [0, 150], histtype='step', density=True)
+axs[0, 2].hist(df_results['Amp3[mV]'].dropna(), bins=binning, range = [0, 150], histtype='step', density=True)
+axs[0, 3].hist(df_results['Amp4[mV]'].dropna(), bins=binning, range = [0, 150], histtype='step', density=True)
+axs[1, 0].hist(df_results['Amp5[mV]'].dropna(), bins=binning, range = [0, 150], histtype='step', density=True)
+axs[1, 1].hist(df_results['Amp6[mV]'].dropna(), bins=binning, range = [0, 150], histtype='step', density=True)
+axs[1, 2].hist(df_results['Amp7[mV]'].dropna(), bins=binning, range = [0, 150], histtype='step', density=True)
+axs[1, 3].hist(df_results['Amp8[mV]'].dropna(), bins=binning, range = [0, 150], histtype='step', density=True)
 
 axs[0,0].set_title('Channel 1')
 axs[0,1].set_title('Channel 2')
